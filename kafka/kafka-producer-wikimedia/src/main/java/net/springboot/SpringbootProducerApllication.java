6367ff714f8591ca46ea9bcf95a13147353c1d2d
Shehan Premathilaka <harshanashehan52@gmail.com>
2023-12-10 12:55:14 +0530

    Thursday

--- kafka/kafka-producer-wikimedia/src/main/java/net/springboot/SpringbootProducerApllication.java
+++ kafka/kafka-producer-wikimedia/src/main/java/net/springboot/SpringbootProducerApllication.java
@@ -1,2 +1,11 @@
-package net.springboot;public class SpringbootProducerApllication {
+package net.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProducerApllication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProducerApllication.class);
+    }
 }
--- kafka/kafka-producer-wikimedia/src/main/java/net/springboot/WikimediaChangerHandler.java
+++ kafka/kafka-producer-wikimedia/src/main/java/net/springboot/WikimediaChangerHandler.java
@@ -1,2 +1,31 @@
-package net.springboot;public class WikimediaChangerHandler {
+package net.springboot;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+
+public class WikimediaChangerHandler implements EventHandler{
+    @Override
+    public void onOpen() throws Exception {
+
+    }
+
+    @Override
+    public void onClosed() throws Exception {
+
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) throws Exception {
+
+    }
+
+    @Override
+    public void onComment(String s) throws Exception {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
 }
